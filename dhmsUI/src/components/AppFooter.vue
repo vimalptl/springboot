3f4f38b497ef4bebbfbb1cbfbd180d60aeec2fdf
Patel <vimalp@ri-net.com>
2019-05-09 16:23:03 -0700

    Remove dhmsui

--- dhmsUI/src/components/AppFooter.vue
+++
@@ -1,9 +0,0 @@
-<template>
-<footer class="footer">
-  <div class="content has-text-centered">
-    <p>
-      <strong>Site was build using Vue and Bulma</strong> by Vimal.
-    </p>
-  </div>
-</footer>
-</template>
